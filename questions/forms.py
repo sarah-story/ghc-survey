866b6c654999fe33f4ef5d539bf4ac83041251d3
Sarah Story <sstory@3442528.local>
2016-01-24 11:34:21 -0600

    some styling

--- questions/forms.py
+++ questions/forms.py
@@ -1,34 +1,42 @@
 from django import forms
+from django.utils.safestring import mark_safe
 
-STATUS_OPTIONS = (("Willing to Participate", 'Willing to Participate'),
+STATUS_OPTIONS = (("", "None Selected"),
+                  ("Willing to Participate", 'Willing to Participate'),
                   ("Not Willing to Participate", 'Not Willing to Participate'),
                   ('Not Home', 'Not Home'))
 
-RATE_1_TO_5 = ((1,1),
+RATE_1_TO_5 = ((-1, "None Selected"),
+               (1,1),
                (2,2),
                (3,3),
                (4,4),
                (5,5))
 
-INVOLVEMENT = (("not involved","not involved"),
+INVOLVEMENT = (("", "None Selected"),
+               ("not involved","not involved"),
                ("not very involved","not very involved"),
                ("somewhat involved","somewhat involved"),
                ("involved","involved"),
                ("very involved","very involved"))
 
-YES_NO = (("yes","yes"),
+YES_NO = (("", "None Selected"),
+          ("yes","yes"),
           ("no","no"))
 
-NOW_LATER = (("now", "now"),
+NOW_LATER = (("", "None Selected"),
+             ("now", "now"),
              ("later", "later"))
 
-SIMILARITY = (("very different", "very different"),
+SIMILARITY = (("", "None Selected"),
+              ("very different", "very different"),
               ("different", "different"),
               ("unsure","unsure"),
               ("similar", "similar"),
               ("very similar", "very similar"))
 
-RELIGIONS = (("Agnosticism","Agnosticism"),
+RELIGIONS = (("", "None Selected"),
+             ("Agnosticism","Agnosticism"),
              ("Atheism","Atheism"),
              ("Buddhism", "Buddhism"),
              ("Christianity", "Christianity"),
@@ -39,21 +47,24 @@ RELIGIONS = (("Agnosticism","Agnosticism"),
              ("Taoism", "Taoism"),
              ("Other", "Other"))
 
-AGES = (("under 18", "under 18"),
+AGES = (("", "None Selected"),
+        ("under 18", "under 18"),
         ("18-30", "18-30"),
         ("31-50", "31-50"),
         ("51-70", "51-70"),
         ("over 70", "over 70"),
         ("prefer to not disclose", "prefer to not disclose"))
 
-RACES = (("American Indian/Alaskan Native", "American Indian/Alaskan Native"),
+RACES = (("", "None Selected"),
+         ("American Indian/Alaskan Native", "American Indian/Alaskan Native"),
          ("Asian", "Asian"),
          ("Black/African American", "Black/African American"),
          ("Native Hawaiian/Other Pacific Islander", "Native Hawaiian/Other Pacific Islander"),
          ("White", "White"),
          ("prefer to not disclose", "prefer to not disclose"))
 
-GENDER = (("Male", "Male"),
+GENDER = (("", "None Selected"),
+          ("Male", "Male"),
           ("Female", "Female"),
           ("prefer to not disclose", "prefer to not disclose"))
 
@@ -62,76 +73,76 @@ class PrelimQuestions(forms.Form):
     street_address = forms.CharField(max_length=100)
     city = forms.CharField(max_length=50)
     zip = forms.IntegerField()
-    status = forms.ChoiceField(widget=forms.Select, choices=STATUS_OPTIONS)
+    status = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}), choices=STATUS_OPTIONS)
 
 
 class ParticipantQuestions(forms.Form):
 
     rate_ghc = forms.ChoiceField(
-        widget=forms.RadioSelect,
+        widget=forms.Select(attrs={'class': 'form-control'}),
         choices=RATE_1_TO_5,
         label="How would you rate your interactions with GHC thus far?",
         required=False
     )
 
     ghc_impact = forms.ChoiceField(
-        widget=forms.RadioSelect,
+        widget=forms.Select(attrs={'class': 'form-control'}),
         choices=RATE_1_TO_5,
         label="How positive is GHC's impact on the neighborhood?",
         required=False
     )
 
     ghc_involvement = forms.ChoiceField(
-        widget=forms.RadioSelect,
+        widget=forms.Select(attrs={'class': 'form-control'}),
         choices=INVOLVEMENT,
         label="How involved do you feel GHC is in your neighborhood?",
         required=False
     )
 
     desired_involvement = forms.ChoiceField(
-        widget=forms.RadioSelect,
+        widget=forms.Select(attrs={'class': 'form-control'}),
         choices=INVOLVEMENT,
         label="How involved would you like GHC to be in your neighborhood?",
         required=False
     )
 
     community_event_interest = forms.ChoiceField(
-        widget=forms.RadioSelect,
+        widget=forms.Select(attrs={'class': 'form-control'}),
         choices=YES_NO,
         label="Would you be interested in attending community events held at GHC?",
         required=False
     )
 
     religious_similarity = forms.ChoiceField(
-        widget=forms.RadioSelect,
+        widget=forms.Select(attrs={'class': 'form-control'}),
         choices=SIMILARITY,
         label="How would you say GHC's religious beliefs compare to yours?",
         required=False
     )
 
     religious_identification = forms.ChoiceField(
-        widget=forms.RadioSelect,
+        widget=forms.Select(attrs={'class': 'form-control'}),
         choices=RELIGIONS,
         label="Do you identify with a major world religion?",
         required=False
     )
 
     age_range = forms.ChoiceField(
-        widget=forms.RadioSelect,
+        widget=forms.Select(attrs={'class': 'form-control'}),
         choices=AGES,
         label="What is your age range",
         required=False
     )
 
     race_ethnicity = forms.ChoiceField(
-        widget=forms.RadioSelect,
+        widget=forms.Select(attrs={'class': 'form-control'}),
         choices=RACES,
         label="What is your race/ethnicity?",
         required=False
     )
 
     gender = forms.ChoiceField(
-        widget=forms.RadioSelect,
+        widget=forms.Select(attrs={'class': 'form-control'}),
         choices=GENDER,
         label="What is your gender?",
         required=False
@@ -141,14 +152,14 @@ class ParticipantQuestions(forms.Form):
 class FollowUpQuestions(forms.Form):
 
     follow_up = forms.ChoiceField(
-        widget=forms.RadioSelect,
+        widget=forms.Select(attrs={'class': 'form-control'}),
         choices=YES_NO,
         label="Would you be interested in learning more about what we believe at Green Hills",
         required=False
     )
 
     witnessed_to = forms.ChoiceField(
-        widget=forms.RadioSelect,
+        widget=forms.Select(attrs={'class': 'form-control'}),
         choices=NOW_LATER,
         label="If yes, do you have time now, or should we contact you later?",
         required=False
