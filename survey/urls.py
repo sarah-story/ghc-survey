8503702527d939435778c13d59aebd103b672cda
storysj <sean.story@digitalreasoning.com>
2016-01-31 21:36:44 -0600

    little fixes

--- survey/urls.py
+++ survey/urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'questions.views.index', name='index'),
     url(r'^survey/$', 'questions.views.index', name='index'),
     url(r'^survey/2', 'questions.views.page_two', name='page_two'),
     url(r'^survey/done', 'questions.views.done', name='done'),
